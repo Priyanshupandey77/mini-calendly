350079d24155cefc01ec8008e7e330403518a38b
Priyanshu Pandey <pandeypriyanshu7777@gmail.com>
2026-09-25 18:05:07 +0530

    feat: handle expired auth sessions

--- frontend/src/components/auth/ProtectedRoute.tsx
+++ frontend/src/components/auth/ProtectedRoute.tsx
@@ -2,20 +2,18 @@ import { Navigate } from "react-router-dom";
 import { useAuth } from "../../contexts/AuthContext";
 import type { ReactNode } from "react";
 
-
 interface ProtectedRouteProps {
   children: ReactNode;
 }
 
-export default function ProtectedRoute({children}: ProtectedRouteProps) {
+export default function ProtectedRoute({ children }: ProtectedRouteProps) {
   const { isLoading, isAuthenticated } = useAuth();
 
   if (isLoading) {
     return <h1>Loading...</h1>;
   }
   if (!isAuthenticated) {
-    return <Navigate to="/login"  />
+    return <Navigate to="/login" />;
   }
   return children;
-
 }
